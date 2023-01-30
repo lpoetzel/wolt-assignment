c5a91fec1c4b586d4070eb6f02465c163c019c65
Lars Poetzel <lars.poetzel@gmail.com>
2023-01-30 18:10:11 +0100

    function in seperate file

--- src/components/DeliveryFeeInput.tsx
+++ src/components/DeliveryFeeInput.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { DeliveryFeeContext } from 'src/contexts/DeliveryFee';
+import { calculateFee } from 'src/functions/DeliveryFeeCalculator';
 import Button from './Button';
 import Form from './Form';
 import Input from './Input';
@@ -17,40 +18,6 @@ export const DeliveryFeeCalculator: React.FC = () => {
         orderTime,
         setOrderTime,
     } = useContext(DeliveryFeeContext);
-
-    const calculateFee = (e: React.FormEvent<EventTarget>) => {
-        e.preventDefault()
-        let fee = 0;
-        // Small order surcharge
-        if (cartValue < 10) {
-            fee += 10 - cartValue;
-        }
-        // Delivery distance fee
-        if (distance <= 1000) {
-            fee += 2;
-        } else {
-            fee += 2 + Math.ceil((distance - 1000) / 500);
-        }
-        // Number of items fee
-        if (numItems >= 5) {
-            let surcharge = (numItems - 4) * 0.5;
-            if (numItems > 12) {
-                surcharge += 1.2;
-            }
-            fee += surcharge;
-        }
-        // Friday rush fee between 15 and 19 UTC
-        if (orderTime.getUTCDay() === 5 && orderTime.getUTCHours() >= 15 && orderTime.getUTCHours() < 19) {
-            fee *= 1.2;
-        }
-        // Max fee
-        fee = Math.min(fee, 15);
-        // Free delivery starting at 100€
-        if (cartValue >= 100) {
-            fee = 0;
-        }
-        setFee(Math.round(fee * 100) / 100);
-    };
     return (
         <Form>
             <Input
@@ -79,7 +46,7 @@ export const DeliveryFeeCalculator: React.FC = () => {
                 onChange={(e) => setOrderTime(new Date(e.target.value))}
             />
 
-            <Button onClick={calculateFee} buttonText="Calculate delivery price" />
+            <Button onClick={(e) => calculateFee(e, cartValue, distance, numItems, orderTime, setFee)} buttonText="Calculate delivery price" />
             <h3>Delivery fee: {fee} €</h3>
         </Form>
     );

620b0ac2d63f0adc4df236954795ad1f07cca2fe
Lars Poetzel <lars.poetzel@gmail.com>
2023-02-05 13:02:51 +0100

    variables in calculateFee

--- src/functions/calculateFee.ts
+++ src/functions/calculateFee.ts
@@ -1,3 +1,21 @@
+const smallOrder = 10;
+
+const minDistance = 1000;
+const minDistanceFee = 2;
+
+const itemFeeStart = 5;
+
+const bulkFeePerItem = 0.5;
+const bulkNumber = 12;
+const bulkNumberSurcharge = 1.2;
+
+const rushDay = 5; //for friday, 5th day of the week
+const rushStart = 15;
+const rushEnd = 19;
+const rushFeeMultiplier = 1.2;
+
+const maxFee = 15;
+const freeDelivery = 100;
 export const calculateFee = (
   e: any,
   cartValue: number,
@@ -8,40 +26,42 @@ export const calculateFee = (
 ): void => {
   e.preventDefault();
   let fee = 0;
-  // Small order surcharge
-  if (cartValue < 10) {
-    fee += 10 - cartValue;
+  // Small order surcharge for less than 10 € atm
+  if (cartValue < smallOrder) {
+    fee += smallOrder - cartValue;
   }
   // Delivery distance fee
-  if (distance <= 1000) {
-    fee += 2;
+  if (distance <= minDistance) {
+    fee += minDistanceFee;
   } else {
-    fee += 2 + Math.ceil((distance - 1000) / 500);
+    fee += minDistanceFee + Math.ceil((distance - minDistance) / 500);
   }
   // Number of items fee
-  if (numItems >= 5) {
-    let surcharge = (numItems - 4) * 0.5;
-    if (numItems > 12) {
-      surcharge += 1.2;
+  if (numItems >= itemFeeStart) {
+    let surcharge = (numItems - (itemFeeStart - 1)) * bulkFeePerItem;
+    if (numItems > bulkNumber) {
+      surcharge += bulkNumberSurcharge;
     }
     fee += surcharge;
   }
   // Friday rush fee between 15 and 19 UTC
   if (
-    orderTime.getUTCDay() === 5 &&
-    orderTime.getUTCHours() >= 15 &&
-    orderTime.getUTCHours() < 19
+    orderTime.getUTCDay() === rushDay &&
+    orderTime.getUTCHours() >= rushStart &&
+    orderTime.getUTCHours() < rushEnd
   ) {
-    fee *= 1.2;
+    fee *= rushFeeMultiplier;
   }
   // Max fee
-  fee = Math.min(fee, 15);
+  fee = Math.min(fee, maxFee);
   // Free delivery starting at 100€
-  if (cartValue >= 100) {
+  if (cartValue >= freeDelivery) {
     fee = 0;
   }
+  //setting the fee to 0 in case parameters are missing
   if (cartValue === 0 || numItems === 0 || distance === 0) {
     fee = 0;
   }
+  //rounding to two decimals
   setFee(Math.round(fee * 100) / 100);
 };
